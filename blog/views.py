db1e47c508c16b7e0c8f2c607f909db150b6db6d
unknown <fahim.web05@gmail.com>
2025-02-16 22:17:01 +0600

    updated- pagination, category page

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog, Category
 from django.db.models import Count
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def home(request):
     blogs = Blog.objects.all().order_by('-created_at')
-    return render(request, 'home.html', {'blogs': blogs})
+    paginator = Paginator(blogs, 10)
+    page_number = request.GET.get('page')
+    page_blog = paginator.get_page(page_number)
+    return render(request, 'home.html', { 'page_blog': page_blog})
 
 def blogpost(request, slug):  # ✅ Accept `slug` as a function parameter
     blog = get_object_or_404(Blog, slug=slug)  # ✅ Filter the blog using slug
@@ -27,7 +31,8 @@ def blogpost(request, slug):  # ✅ Accept `slug` as a function parameter
 def category_details(request, slug):
     category = get_object_or_404(Category, slug=slug)
     categories = Category.objects.all()
-    return render(request, 'category.html', {'category': category})
+    cat_blogs = Blog.objects.filter(category=category, status="published").order_by('-created_at')
+    return render(request, 'category.html', {'category': category, 'cat_blogs': cat_blogs})
 
 
 def about(request):
